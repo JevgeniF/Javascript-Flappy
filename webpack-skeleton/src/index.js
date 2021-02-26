557836cac34b1a092ae4b72ed8ee2590a77b1853
Jevgeni <jefenk@taltech.ee>
2021-02-26 11:38:13 +0200

    webpack

--- webpack-skeleton/src/index.js
+++ webpack-skeleton/src/index.js
@@ -1,3 +1,24 @@
 import msg from './contents.js'
 
-document.write(msg);
+//document.write(msg);
+//alert(msg);
+
+var div = document.createElement('div');
+div.id = 'app';
+div.innerHTML = msg;
+
+document.body.appendChild(div);
+
+console.log();
+
+//var appDiv = document.querySelector("#app");
+
+//console.log(appDiv);
+
+//var f = () => {
+//    appDiv.innerHTML = msg;
+//}
+
+//setTimeout(f, 5000)
+
+//appDiv.innerHTML = msg;
